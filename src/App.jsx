e80e0ca8303d5606c9d91c9e59d5f1c84419d776
Banuka Bandara <banupawan12@gmail.com>
2026-02-18 17:04:13 +0530

    scrollup

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,21 @@ import Venue from "./pages/Venue.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/dining" element={<Dining />} />
-        <Route path="/dining/:id" element={<Venue />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+    <ScrollToTop>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/dining" element={<Dining />} />
+          <Route path="/dining/:id" element={<Venue />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ScrollToTop>
+  ); // Added missing closing tag and brace
 }
